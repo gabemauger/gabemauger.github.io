09f877a730581d566f40661fc2e75699f6afad07
Gabriel Mauger <65745023+gabemauger@users.noreply.github.com>
2022-02-28 00:35:12 -0700

    Update checkersscript.js

--- checkersscript.js
+++ checkersscript.js
@@ -1,3 +1,4 @@
+//constructing the board
 const board = [
   null, 0, null, 1, null, 2, null, 3,
   4, null, 5, null, 6, null, 7, null,
@@ -8,3 +9,14 @@ const board = [
   null, 16, null, 17, null, 18, null, 19,
   20, null, 21, null, 22, null, 23, null
 ]
+
+const cells = document.querySelectorAll("td");
+let redP = document.querySelectorAll("p");
+let blackP = document.querySelectorAll("span");
+const redTurnText = document.querySelectorAll("reds-turn");
+const blackTurnText = document.querySelectorAll("blacks-turn");
+
+let turn = true;
+let redScore = 12;
+let blackScore = 12;
+let playerPieces;
